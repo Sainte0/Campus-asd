64ec80cfa767e761f0e8a405704383deb9ea5676
Sainte <s.santo2004@gmail.com>
2025-03-26 23:02:19 -0300

    fix login

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,23 +1,27 @@
 import 'next-auth';
 import { DefaultSession } from 'next-auth';
+import NextAuth from 'next-auth';
 
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      id: string;
-      role: string;
-    } & DefaultSession['user']
-  }
-
   interface User {
     role: string;
-    id: string;
+    documento: string;
+    eventId: string;
+  }
+
+  interface Session {
+    user: {
+      role: string;
+      documento: string;
+      eventId: string;
+    } & DefaultSession['user']
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
     role: string;
-    id: string;
+    documento: string;
+    eventId: string;
   }
 } 
