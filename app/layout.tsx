c9d8a0e099fb92d8170e50703d3cc9f61b0cba71
Sainte <s.santo2004@gmail.com>
2025-03-28 20:26:55 -0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/asd.jpeg" type="image/jpeg" />
       </head>
       <body className={inter.className}>
         <Toaster position="top-center" />
